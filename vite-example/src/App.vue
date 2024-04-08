acb04fbd31bef2fc9fe61a1a7769c0441438a469
ksaJunmin <23-089@ksa.hs.kr>
2024-04-08 13:44:33 +0900

    vite-example

---
+++ vite-example/src/App.vue
@@ -0,0 +1,17 @@
+<template lang="">
+	<div> {{ count }} </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			count: 0,
+		}
+	}
+}
+</script>
+
+<style lang="">
+  
+</style>
