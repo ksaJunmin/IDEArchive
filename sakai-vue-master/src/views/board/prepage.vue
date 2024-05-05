3d4b494e141876ddd0a05885f758249a31b9acbe
Jeongwooksa <hongjeongwoo0628@gmail.com>
2024-05-05 23:48:12 +0900

    prepage

---
+++ sakai-vue-master/src/views/board/prepage.vue
@@ -0,0 +1,46 @@
+
+<template>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>IDEARCHIVE - 아이디어 공유 게시판</title>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #f8f8f8;
+                text-align: center;
+            }
+
+            .logo {
+                margin-top: 50px;
+                font-size: 3rem;
+                font-weight: bold;
+                color: #333;
+            }
+
+            .slogan {
+                font-size: 1.5rem;
+                color: #666;
+                margin-bottom: 50px;
+            }
+
+            .highlight {
+                color: #ff5733; /* A를 강조하는 색상 */
+            }
+        </style>
+
+</head>
+
+<body>
+    <div class="logo">
+        IDE<em class="highlight">A</em>RCHIVE
+    </div>
+    <div class="slogan">
+        다양한 아이디어를 공유하는 게시판
+    </div>
+</body>
+
+
+</template>
