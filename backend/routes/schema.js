f1ce75cb2fcc00719b86a2d000c2631631ad3fc4
ksaJunmin <23-089@ksa.hs.kr>
2024-05-19 03:02:49 +0900

    qw

--- backend/routes/schema.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const userSchema = new mongoose.Schema({
-  id: String,
-  data: String,
-});
-
-const User = mongoose.model('User', userSchema);
-export default User;
