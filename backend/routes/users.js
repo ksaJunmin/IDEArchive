a75cbd621dcf6c614de3dbe1256dee51d52780e8
ksaJunmin <23-089@ksa.hs.kr>
2024-05-19 15:43:16 +0900

    asdfj

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,11 +3,9 @@ import express from 'express';
 const router = express.Router();
 import User from '../db/user.js';
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({extended:false}));
 
 
-/*
 router.post('/', function(req,res){
   console.log('served '+req.body.name+' '+req.body.schoolID+' '+req.body.email+' '+req.body.password);
   User.create({
@@ -20,23 +18,4 @@ router.post('/', function(req,res){
 router.get('/', function(req,res,next){
   res.send('asdf');
 })
-*/
-
-router.post('/user', async (req, res) => {
-  try {
-    const { name, schoolID, email, password } = req.body;
-    console.log('served', name, schoolID, email, password);
-    const newUser = new User({
-      name,
-      schoolID,
-      email,
-      password
-    });
-    await newUser.save();
-    res.status(201).send('User created');
-  } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
 export default router;
--- frontend/src/views/board/signin.vue
+++ frontend/src/views/board/signin.vue
@@ -1,62 +1,22 @@
-<script setup>
+<script setup>import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import axios from 'axios';
-import { ref } from 'vue';
-
-
-const name = ref('');
-const schoolID = ref('');
-const email = ref('');
-const password = ref('');
-
-const registerUser = async () => {
-  try {
-    const response = await axios.post('/users', {
-      name: name.value,
-      schoolID: schoolID.value,
-      email: email.value,
-      password: password.value
-    });
-    console.log(response.data);
-    alert('회원가입이 완료되었습니다.');
-    router.push('/');
-  } catch (error) {
-    console.error('회원가입 중 오류가 발생했습니다:', error);
-    alert('회원가입에 실패했습니다.');
-  }
-};
 const router = useRouter();
-const goToMain = () => {
-    
-    router.push('/');
-};
-</script>
 
-<script>
+const form = ref({
+  name: '',
+  schoolID: '',
+  email: '',
+  password: ''
+});
 
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        schoolID: '',
-        email: '',
-        password: ''
-      }
-    };
-  },
-  methods: {
-    async register() {
-      try {
-        const response = await axios.post('http://localhost:3000/users', this.form);
-        alert(response.data);
-        router.push('/');
-      } catch (error) {
-        alert('Error registering user: ' + error.response.data);
-      }
-    }
+const register = async () => {
+  try {
+    const response = await axios.post('http://localhost:3000/users', form.value);
+    alert(response.data);
+    router.push('/');  // 홈페이지 경로로 이동
+  } catch (error) {
+    alert('사용자 등록 오류: ' + (error.response ? error.response.data : error.message));
   }
 };
 </script>
@@ -65,7 +25,6 @@ export default {
     <div class="grid">
         <div class="col-12 md:col-12">
             <div class="card p-fluid">
-<<<<<<< Updated upstream
                 <form @submit.prevent="register">
                     <h5>회원가입</h5>
                     <div class="field">
@@ -86,26 +45,6 @@ export default {
                     </div>
                     <Button label="확인" class="w-full p-3 text-xl" type="submit"></Button>
                     </form>
-=======
-                <h5>회원가입</h5>
-                <div class="field">
-                    <label for="name1">이름</label>
-                    <InputText id="name1" type="text" />
-                </div>
-                <div class="field">
-                    <label for="email1">학번</label>
-                    <InputText id="id1" type="text" />
-                </div>
-                <div class="field">
-                    <label for="age1">이메일</label>
-                    <InputText id="email1" type="text" />
-                </div>
-                <div class="field">
-                    <label for="age1">비밀번호</label>
-                    <Password v-model="value" :feedback="false" variant="filled" label="비밀번호를 입력하세요" />
-                </div>
-                <Button label="확인" class="w-full p-3 text-xl" @click="registerUser()"></Button>
->>>>>>> Stashed changes
             </div>
         </div>
     </div>
