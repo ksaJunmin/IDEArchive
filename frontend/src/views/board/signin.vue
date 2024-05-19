0011ba4db6ed789c6c463c3bba4040c2bbe4db8a
Jeongwooksa <hongjeongwoo0628@gmail.com>
2024-05-20 03:20:20 +0900

    signinerror

--- frontend/src/views/board/signin.vue
+++ frontend/src/views/board/signin.vue
@@ -35,6 +35,14 @@ const validateForm = () => {
     errors.value.schoolID = '';
   }
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(form.value.email)) {
+    errors.value.email = '유효한 이메일 주소를 입력하세요';
+    valid = false;
+  } else {
+    errors.value.email = '';
+  }
+
   if (form.value.password.length < 8) {
     errors.value.password = '비밀번호는 8자 이상이어야 합니다';
     valid = false;
@@ -56,7 +64,7 @@ const register = async () => {
   try {
     router.push('/');  // 홈페이지 경로로 이동
     const response = await axios.post('http://localhost:3000/users', form.value);
-    alert(response.data);
+    //alert(response.data);
   } catch (error) {
     alert('사용자 등록 오류: ' + (error.response ? error.response.data : error.message));
     
@@ -65,30 +73,38 @@ const register = async () => {
 </script>
 
 <template>
-    <div class="grid">
-        <div class="col-12 md:col-12">
-            <div class="card p-fluid">
-                <form @submit.prevent="register">
-                    <h5>회원가입</h5>
-                    <div class="field">
-                        <label for="name">이름</label>
-                        <InputText id="name" v-model="form.name" type="text" />
-                    </div>
-                    <div class="field">
-                        <label for="schoolID">학번</label>
-                        <InputText id="schoolID" v-model="form.schoolID" type="text" />
-                    </div>
-                    <div class="field">
-                        <label for="email">이메일</label>
-                        <InputText id="email" v-model="form.email" type="text" />
-                    </div>
-                    <div class="field">
-                        <label for="password">비밀번호</label>
-                        <Password v-model="form.password" :feedback="false" variant="filled" label="비밀번호를 입력하세요" />
-                    </div>
-                    <Button label="확인" class="w-full p-3 text-xl" type="submit"></Button>
-                </form>
-            </div>
+  <div class="grid">
+    <div class="col-12 md:col-12">
+      <div class="card p-fluid">
+        <form @submit.prevent="register">
+          <h5>회원가입</h5>
+          <div class="field">
+            <label for="name">이름</label>
+            <InputText id="name" v-model="form.name" type="text" />
+            <small v-if="errors.name" class="p-error">{{ errors.name }}</small>
+          </div>
+          <div class="field">
+            <label for="schoolID">학번</label>
+            <InputText id="schoolID" v-model="form.schoolID" type="text" />
+            <small v-if="errors.schoolID" class="p-error">{{ errors.schoolID }}</small>
+          </div>
+          <div class="field">
+            <label for="email">이메일</label>
+            <InputText id="email" v-model="form.email" type="text" />
+            <small v-if="errors.email" class="p-error">{{ errors.email }}</small>
+          </div>
+          <div class="field">
+            <label for="password">비밀번호</label>
+            <Password v-model="form.password" :feedback="false" variant="filled" label="비밀번호를 입력하세요" />
+            <small v-if="errors.password" class="p-error">{{ errors.password }}</small>
+          </div>
+          <Button label="확인" class="w-full p-3 text-xl" type="submit"></Button>
+        </form>
+        <div v-if="errorMessage" class="error-message">
+          {{ errorMessage }}
         </div>
+      </div>
     </div>
+  </div>
+
 </template>
