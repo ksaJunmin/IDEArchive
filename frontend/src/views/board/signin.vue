77d3086ea4bed4d865f9afa7fa43fdec015ca0af
Jeongwooksa <hongjeongwoo0628@gmail.com>
2024-05-19 14:59:46 +0900

    restore

--- frontend/src/views/board/signin.vue
+++ frontend/src/views/board/signin.vue
@@ -1,7 +1,33 @@
 <script setup>
 import { useRouter } from 'vue-router';
+import axios from 'axios';
+import { ref } from 'vue';
+
+
+const name = ref('');
+const schoolID = ref('');
+const email = ref('');
+const password = ref('');
+
+const registerUser = async () => {
+  try {
+    const response = await axios.post('/users', {
+      name: name.value,
+      schoolID: schoolID.value,
+      email: email.value,
+      password: password.value
+    });
+    console.log(response.data);
+    alert('회원가입이 완료되었습니다.');
+    router.push('/');
+  } catch (error) {
+    console.error('회원가입 중 오류가 발생했습니다:', error);
+    alert('회원가입에 실패했습니다.');
+  }
+};
 const router = useRouter();
 const goToMain = () => {
+    
     router.push('/');
 };
 </script>
@@ -39,6 +65,7 @@ export default {
     <div class="grid">
         <div class="col-12 md:col-12">
             <div class="card p-fluid">
+<<<<<<< Updated upstream
                 <form @submit.prevent="register">
                     <h5>회원가입</h5>
                     <div class="field">
@@ -59,6 +86,26 @@ export default {
                     </div>
                     <Button label="확인" class="w-full p-3 text-xl" type="submit"></Button>
                     </form>
+=======
+                <h5>회원가입</h5>
+                <div class="field">
+                    <label for="name1">이름</label>
+                    <InputText id="name1" type="text" />
+                </div>
+                <div class="field">
+                    <label for="email1">학번</label>
+                    <InputText id="id1" type="text" />
+                </div>
+                <div class="field">
+                    <label for="age1">이메일</label>
+                    <InputText id="email1" type="text" />
+                </div>
+                <div class="field">
+                    <label for="age1">비밀번호</label>
+                    <Password v-model="value" :feedback="false" variant="filled" label="비밀번호를 입력하세요" />
+                </div>
+                <Button label="확인" class="w-full p-3 text-xl" @click="registerUser()"></Button>
+>>>>>>> Stashed changes
             </div>
         </div>
     </div>
